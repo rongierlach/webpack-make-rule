a9ce70579e230cb991970352b73b4b38293197b9
Ron Gierlach <rongierlach@gmail.com>
2017-01-17 21:06:30 -0600

    better condition handling

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-/**
- * Enum string values.
- * @enum {string}
+const isString = value =>
+  typeof value === 'string'
+
+const isArray = value =>
+  Array.isArray(value)
+
+// https://webpack.js.org/configuration/module/#condition
+const normalizeTest = ext => {
+  if (!ext) throw new TypeError('You must pass an extension as an argument to makeRule')
+  return isString(ext) ? RegExp(`/.${ext}$/`) : ext
+}
+
+const normalizeLoader = (loader, ext) => {
+  if (isArray(loader)) return loader
+  if (isString(loader)) return [ loader ]
+  return [ loader || `${ext}-loader` ]
+}
 
 /** @function
  * @name makeRule
  * @desc Returns a Webpack rule configuration object
- * @param {object|string} ext
- * @param {string} loader
+ * @param {string|object} ext
+ * @param {string|object|Array.<string|object>} loader
  * @param {string} enforce
- * @param {string} exclude
- * @returns {object} Webpack rule configuration
- * @example const rules = [
- *   makeRule(/\.jsx?$/, 'standard-loader', 'pre'),
- *   makeRule(/\.jsx?$/, 'babel-loader'),
- *   makeRule('json')
- * ]
+ * @param {string|object|Array.<string|object>} exclude
+ * @param {string|object|Array.<string|object>} include
  */
 export default (ext, loader, enforce, exclude, include) => ({
-  test: typeof ext === 'string'
-    ? RegExp(`/.${ext}$/`)
-    : ext,
-  use: typeof loader === 'string'
-    ? loader
-    : loader || `${ext}-loader`,
+  test:
+    normalizeTest(ext),
+  use:
+    normalizeLoader(loader, ext),
   enforce:
     enforce || undefined,
   exclude:
